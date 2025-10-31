472007ecd9e9e902bec0b32c282d567f8bd30b34
korendn-sys <korendn@gmail.com>
2025-10-31 15:23:37 +0200

    Fix formatting and add import for math module

--- math_utils.py
+++ math_utils.py
@@ -9,8 +9,8 @@ def find_mean(num1, num2, num3):
     mean = (num1 + num2 + num3) / 3
     return mean
 
+import math
 def find_mean_std(num1, num2, num3):
     mean = find_mean(num1, num2, num3)
     std = math.sqrt(((num1 - mean)**2 + (num2 - mean)**2 + (num3 - mean)**2) / 3)
     return mean, std
-
